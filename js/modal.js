57328b041059ff43ba38446725b5f1534176838b
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-03-06 22:36:48 -0500

    Link suits page, fix modal postitioning bug

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,12 @@
 var $btnUpNext = $('.btn-up-next')
 var $btnCloseModal = $('.btn-close-modal');
-var $modal = $('.modal');
 var $body = $('body');
+var $modal = $('.modal');
 
 $btnUpNext.click(function () {
+  var offset = $(document).scrollTop();
+  var viewportHeight = $(window).height()
+  $modal.css('top',  (offset  + (viewportHeight/2)) - ($modal.outerHeight()/2));
   $modal.show();
   $body.css('overflow','hidden');
 });
